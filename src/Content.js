1db70107beb8a72fdad1651dc9f39a459b920c5c
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-10-31 21:39:21 +0100

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 const Content = () => {
+  const [name, setName] = useState("Mirna");
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ["Mirna", "Ana", "Ivana"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
 
   const handleClick = () => {
-    console.log("You clicked it");
+    setCount(count + 1);
+    setCount(count + 1);
+    console.log(count);
   };
 
-  const handleClick2 = (name) => {
-    console.log(`${name} was clicked`);
+  const handleClick2 = () => {
+    console.log(count);
   };
 
   const handleClick3 = (e) => {
@@ -21,10 +27,10 @@ const Content = () => {
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      <button onClick={handleNameChange}>Change Name</button>
       <button onClick={handleClick}>Click It</button>
-      <button onClick={() => handleClick2("Mirna")}>Click It</button>
-      <button onClick={(e) => handleClick3(e)}>Click It</button>
+      <button onClick={handleClick2}>Click It</button>
     </main>
   );
 };
